fdfa9e507b4a28dd2ac64699e48df28f6c6e0ec9
eyal462 <eyal462@gmail.com>
2023-01-12 16:15:29 +0200

    NEW CARDS

--- BerkPCK/Card.py
+++ BerkPCK/Card.py
@@ -4,10 +4,38 @@ class Card:
         self.suit = suit
 
     def __repr__(self):
-        return f"CARD {self.value}, {self.suit}\n"
+        card = ""
+        suit = ""
+        if self.value == 1 or self.value >= 11:
+            if self.value == 1:
+                card = "ACE (1)"
+            if self.value == 11:
+                card = "JACK (11)"
+            if self.value == 12:
+                card = "QUEEN (12)"
+            if self.value == 13:
+                card = "KING (13)"
+
+        if self.suit == 1:
+            suit = "DIMOND (1)"
+        if self.suit == 2:
+            suit = "SPADE (2)"
+        if self.suit == 3:
+            suit = "HEART (3)"
+        if self.suit == 4:
+            suit = "CLUB (4)"
+        if 1 < self.value < 11:
+            return f"CARD {self.value}, {suit}"
+        else:
+            return f"CARD {card}, {suit}"
+
 
     def __gt__(self, other):
         """compare the cards, if current card is higher from ther other, it returns True"""
+        if self.value == 1 and other.value != 1:   # ACE is the highest card
+            return True
+        if self.value != 1 and other.value == 1:
+            return False
         if self.value > other.value:
             return True
         if self.value == other.value and self.suit > other.suit:
--- BerkPCK/MainWarGame.py
+++ BerkPCK/MainWarGame.py
@@ -4,9 +4,9 @@ from CardGame import CardGame
 from Deck_of_Cards import Deck_of_Cards
 
 if __name__ == '__main__':
-    """THE MAIN GAME"""
-  player1=Player("eyal",num_of_cards=26)
-  player2=Player("lior",num_of_cards=26)
+  #  """THE MAIN GAME"""
+  player1=Player("eyal", 26)
+  player2=Player("lior", 26)
   points1 = 0
   points2 = 0
   num_cards = int(input("enter number of cards:"))
@@ -15,7 +15,7 @@ if __name__ == '__main__':
   print(f"player 2 {player2.num_of_cards}")
 
   for i in range (10):
-      """create 10 rounds of War of Cards"""
+   #   """create 10 rounds of War of Cards"""
     card1 = player1.get_card()
     card2 = player2.get_card()
     if card1.__gt__(card2):
@@ -25,7 +25,7 @@ if __name__ == '__main__':
         print(f"{player1.name}'s: {card1.__repr__()}")
         print(f"{player2.name}'s: {card2.__repr__()}")
         points1 +=1
-        print(f"PLAYER 1 ({player1.name})(Points: {player1.name} {points1}:{points2} {player2.name}) IS THE WINNER")
+        print(f"PLAYER 1 ({player1.name}) IS THE WINNER (Points: {player1.name} {points1}:{points2} {player2.name})")
     if card2.__gt__(card1):
         """PLAYER 2 WIN"""
         print("==============NEW MATCH=====================")
@@ -33,7 +33,9 @@ if __name__ == '__main__':
         print(f"{player1.name}'s: {card1.__repr__()}")
         print(f"{player2.name}'s: {card2.__repr__()}")
         points2 += 1
-        print(f"PLAYER 2 ({player2.name})(Points: {player1.name} {points1}:{points2} {player2.name}) IS THE WINNER")
+        print(f"PLAYER 2 ({player2.name}) IS THE WINNER (Points: {player1.name} {points1}:{points2} {player2.name})")
+    else:
+        print("============TIE================")
   print("================END MATCH======================")
   if points1 > points2:
       print(f"PLAYER 1 ({player1.name}) IS THE WINNER")
