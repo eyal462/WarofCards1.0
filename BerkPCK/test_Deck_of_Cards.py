b1d31de73b1917cbdb9517c3cd7c3586cc100d14
eyal462 <eyal462@gmail.com>
2023-01-13 21:04:40 +0200

    -Added testing for Player and Deck of Cards -Added documentation

--- BerkPCK/test_Deck_of_Cards.py
+++ BerkPCK/test_Deck_of_Cards.py
@@ -1,9 +1,28 @@
 from unittest import TestCase
-
+from Deck_of_Cards import Deck_of_Cards
+from Card import Card
 
 class TestDeck_of_Cards(TestCase):
-    def test_cards_shuffle(self):
-        self.fail()
+
+    def setUp(self):
+        self.pack = Deck_of_Cards()
+
+    def test_init(self):
+        """Test for the initialize of deck of cards
+        check if it 52 cards and each card is actually card"""
+        self.assertEqual(len(self.pack.pack), 52)
+        for i in range(len(self.pack.pack)):
+            self.assertIsInstance(self.pack.pack[i], Card)
+
+
 
     def test_deal_one(self):
-        self.fail()
+        """Test for if its a card and if its remove one from the pack"""
+        self.assertIsInstance(self.pack.deal_one(), Card)
+        self.assertEqual(len(self.pack.pack), 51)
+
+    def test_shuffle(self):
+        """check if the pack is orginzed diffrent from the original deck of cards"""
+        list1 = self.pack.pack
+        self.pack.cards_shuffle()
+        self.assertNotEqual(self.pack.pack, list1)
--- BerkPCK/test_Player.py
+++ BerkPCK/test_Player.py
@@ -1,12 +1,25 @@
 from unittest import TestCase
+from Player import Player
+from Card import Card
 
 
 class TestPlayer(TestCase):
+
+    def setUp(self):
+        self.player = Player("eyal", 26)
+
+
     def test_set_hand(self):
-        self.fail()
+        """test for correct number of cards"""
+        for i in range(0, 10):                  # if it's lower than 10,
+            player = Player("Eyal", i)          # its become the default (26)
+            self.assertEqual(player.num_of_cards, 26)
 
     def test_get_card(self):
-        self.fail()
+        self.assertEqual(self.player.get_card(), 1)         # check if its only 1 card
+        self.assertIsInstance(self.player.get_card(), Card) # check if its a card
+        self.assertEqual(self.player.pack_player, 25)
+
 
     def test_add_card(self):
         self.fail()
