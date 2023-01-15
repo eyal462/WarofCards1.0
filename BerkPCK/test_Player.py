33a21d4e154740bc2ade02638c24ef57a004427c
eyal462 <eyal462@gmail.com>
2023-01-15 10:53:31 -0800

    -Added testing for all classes

--- BerkPCK/test_Player.py
+++ BerkPCK/test_Player.py
@@ -14,6 +14,7 @@ class TestPlayer(TestCase):
         self.pack = Deck_of_Cards
 
     def test__init__(self):
+        """test the initialize of player"""
         self.assertEqual(self.player.name, "eyal")
         self.assertEqual(self.player.num_of_cards, 26)
 
